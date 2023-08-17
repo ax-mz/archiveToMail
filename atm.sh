46f3a47d14e1e5c3fb4e08d13610da20958075cb
2AM <126387519+ax-mz@users.noreply.github.com>
2023-08-17 15:02:57 +0200

    Update atm.sh

--- atm.sh
+++ atm.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-if [ -z $1 ];
+DEPS=(curl jq)
+
+for dep in ${DEPS[@]}; do
+    if ! command -v $dep >/dev/null; then
+        MISSING_DEPS+="$dep "
+    fi
+done
+
+if [[ ${#MISSING_DEPS[@]} -ne 0 ]]; then
+    echo "Missing packet(s): ${MISSING_DEPS[@]}"
+    exit 1
+fi
+
+if [[ -z $1 ]];
 then
     echo "Usage : $(basename $0) [IDENTIFIER]"
     exit 1
+fi
+
+ID=$1
+EMAIL=$(curl -s https://archive.org/metadata/$ID/metadata | jq -r .result.uploader)
+if [[ $EMAIL == "null" ]];
+then
+    echo "\"$ID\" is not a valid identifier"
+    exit 1
+elif [[ -z $EMAIL ]];
+then
+    echo "$(basename $0): Error"
+    exit 1
 else
-    ID=$1
-    EMAIL=$(curl -s https://archive.org/metadata/$ID/metadata | jq -r .result.uploader)
-    if [ $EMAIL == "null" ];
-    then
-        echo "\"$ID\" is not a valid identifier"
-        exit 1
-    elif [ -z $EMAIL ];
-    then
-        echo "$(basename $0): Error"
-        exit 1
-    else
-        echo $EMAIL
-        exit 0
-    fi
+    echo $EMAIL
+    exit 0
 fi
